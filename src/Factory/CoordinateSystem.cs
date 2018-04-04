b629cccf474c36927d87b8cecab02d4caf9740ab
Junio Silva <junio.silvac@gmail.com>
2018-04-04 00:57:57 -0300

    Factory pattern

---
+++ src/Factory/CoordinateSystem.cs
@@ -0,0 +1,8 @@
+namespace DesignPatterns.Factory
+{
+   public enum CoordinateSystem
+   {
+     Cartesian,
+     Polar
+   }
+}
---
+++ src/Factory/Point.cs
@@ -0,0 +1,19 @@
+using System;
+namespace DesignPatterns.Factory
+{
+  public class Point
+  {
+    private double x, y;
+
+    public Point(double x, double y)
+    {
+      this.x = x;
+      this.y = y;      
+    }    
+
+    public override string ToString()
+    {
+      return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+    }
+  }  
+}
