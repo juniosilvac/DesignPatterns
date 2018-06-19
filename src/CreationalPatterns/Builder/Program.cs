0589ad615f2c5033ce4c215077fdf28bcc54cd8c
Junio Silva <junio.silvac@gmail.com>
2018-06-18 23:31:38 -0300

    Builder Fluent

--- src/CreationalPatterns/Builder/Program.cs
+++ src/CreationalPatterns/Builder/Program.cs
@@ -4,14 +4,29 @@ using static System.Console;
 
 namespace  CreationalPatterns.Builder
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-           var builder = new HtmlBuilder("ul");
-           builder.AddChild("li", "hello");
-           builder.AddChild("li", "word");
-           WriteLine(builder.ToString());
+          // Example Builder
+          //  var builder = new HtmlBuilder("ul");
+          //  builder.AddChild("li", "hello");
+          //  builder.AddChild("li", "word");
+          //  WriteLine(builder.ToString());
+
+           // Example Builder Fluent
+            var pb = new PersonBuilder();
+            Person person = pb
+              .Lives
+                .At("123 London Road")
+                .In("London")
+                .WithPostcode("SW12BC")
+              .Works
+                .At("Fabrikam")
+                .AsA("Engineer")
+                .Earning(123000);
+
+            WriteLine(person);
         }
     }
 }
