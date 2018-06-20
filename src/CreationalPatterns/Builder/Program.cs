a12a62176c20d2839fc3181761f1d9068774f0d6
Junio Silva <junio.silvac@gmail.com>
2018-06-20 00:06:07 -0300

    Create Abstract Factory

--- src/CreationalPatterns/Builder/Program.cs
+++ src/CreationalPatterns/Builder/Program.cs
@@ -1,32 +1,31 @@
-﻿using System;
-using CreationalPatterns.Builder.Model;
+﻿using CreationalPatterns.Builder.Model;
 using static System.Console;
 
-namespace  CreationalPatterns.Builder
+namespace CreationalPatterns.Builder
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-          // Example Builder
-          //  var builder = new HtmlBuilder("ul");
-          //  builder.AddChild("li", "hello");
-          //  builder.AddChild("li", "word");
-          //  WriteLine(builder.ToString());
+  public class Program
+  {
+      static void Main(string[] args)
+      {
+        // Example Builder
+        //  var builder = new HtmlBuilder("ul");
+        //  builder.AddChild("li", "hello");
+        //  builder.AddChild("li", "word");
+        //  WriteLine(builder.ToString());
 
-           // Example Builder Fluent
-            var pb = new PersonBuilder();
-            Person person = pb
-              .Lives
-                .At("123 London Road")
-                .In("London")
-                .WithPostcode("SW12BC")
-              .Works
-                .At("Fabrikam")
-                .AsA("Engineer")
-                .Earning(123000);
+          // Example Builder Fluent
+          var pb = new PersonBuilder();
+          Person person = pb
+            .Lives
+              .At("123 London Road")
+              .In("London")
+              .WithPostcode("SW12BC")
+            .Works
+              .At("Fabrikam")
+              .AsA("Engineer")
+              .Earning(123000);
 
-            WriteLine(person);
-        }
-    }
+          WriteLine(person);
+      }
+  }
 }
---
+++ src/CreationalPatterns/Factories/Program.cs
@@ -0,0 +1,18 @@
+﻿using CreationalPatterns.Factories.Model;
+using static System.Console;
+
+namespace CreationalPatterns.Factories
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var machine = new HotDrinkMachine();
+            //var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 300);
+            //drink.Consume();
+
+            IHotDrink drink = machine.MakeDrink();
+            drink.Consume();
+        }
+    }
+}
